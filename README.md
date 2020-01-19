93d5d7cc94275a9d731904add25215511cb8da40
Jackson Ming Hu <huming2207@gmail.com>
2020-01-19 15:26:21 +0800

    Update README

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 Yet another Fairchild (OnSemi) FUSB302 library for ESP32
 
- 
+# License
+
+MIT
+
+# Acknowledgements
+
+Some code and logic are based on: 
+
+- [ReclaimerLab's FUSB302 library](https://github.com/ReclaimerLabs/FUSB302) 
+- [Google ChromiumOS Embedded Controller firmware](https://chromium.googlesource.com/chromiumos/platform/ec/+/master/driver/tcpm).
--- main/pdmancer_main.cpp
+++ main/pdmancer_main.cpp
@@ -1,12 +1,13 @@
-#include <stdio.h>
-#include "sdkconfig.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_system.h>
+#include <esp_log.h>
 #include <drv_fusb302.hpp>
+
+#define TAG "fusb302-main"
 
 extern "C" void app_main(void)
 {
-    drv::fusb302(21, 22, 4);
+    auto fusb302 = device::fusb302(21, 22, 4);
+    fusb302.auto_config_polarity();
 }
