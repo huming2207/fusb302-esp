f1b04e378d17bc802bdafd9cc56c5031080f1457
Jackson Ming Hu <huming2207@gmail.com>
2020-01-21 19:54:56 +0800

    tcpm: add more types

--- main/tcpm/tcpm.hpp
+++ main/tcpm/tcpm.hpp
@@ -12,27 +12,64 @@ namespace proto_def
         PROGRAMMABLE_PDO    = 0b11,
     };
 
-    enum ctrl_pkt_type {
-        GOOD_CRC            = 0b0001,
-        GOTO_MIN            = 0b0010,
-        ACCEPT              = 0b0011,
-        REJECT              = 0b0100,
-        PING                = 0b0101,
-        PS_READY            = 0b0110,
-        GET_SOURCE_CAP      = 0b0111,
-        GET_SINK_CAP        = 0b1000,
-        DR_SWAP             = 0b1001,
-        PR_SWAP             = 0b1010,
-        VCONN_SWAP          = 0b1011,
-        WAIT                = 0b1100,
-        SOFT_RESET          = 0b1101,
-        NOT_SUPPORTED       = 0b10000,
+    enum spec_revision {
+        REV_1_0 = 0b00,
+        REV_2_0 = 0b01,
+        REV_3_0 = 0b10
+    };
+
+    enum pkt_type {
+        // Control packets
+        GOOD_CRC                = 0b00001,
+        GOTO_MIN                = 0b00010,
+        ACCEPT                  = 0b00011,
+        REJECT                  = 0b00100,
+        PING                    = 0b00101,
+        PS_READY                = 0b00110,
+        GET_SOURCE_CAP          = 0b00111,
+        GET_SINK_CAP            = 0b01000,
+        DR_SWAP                 = 0b01001,
+        PR_SWAP                 = 0b01010,
+        VCONN_SWAP              = 0b01011,
+        WAIT                    = 0b01100,
+        SOFT_RESET              = 0b01101,
+        NOT_SUPPORTED           = 0b10000,
         GET_SOURCE_CAP_EXTENDED = 0b10001,
         GET_STATUS              = 0b10010,
         FR_SWAP                 = 0b10011,
         GET_PPS_STATUS          = 0b10100,
         GET_COUNTRY_CODES       = 0b10101,
-        GET_SINK_CAP_EXTENDED   = 0b10110
+        GET_SINK_CAP_EXTENDED   = 0b10110,
+
+        // Data packets
+        SOURCE_CAPABILITIES     = -0b00001,
+        REQUEST                 = -0b00010,
+        BIST                    = -0b00011,
+        SINK_CAPABILITIES       = -0b00100,
+        BATTERY_STATUS          = -0b00101,
+        ALERT                   = -0b00110,
+        GET_COUNTRY_INFO        = -0b00111,
+        ENTER_USB               = -0b01000,
+        VENDOR_DEFINED          = -0b01111
+    };
+
+    enum port_power_role {
+        SINK = 0,
+        SOURCE = 1,
+    };
+
+    enum port_data_role {
+        UFP = 0,
+        DFP = 1
+    };
+
+    struct header {
+        uint8_t num_obj = 0;
+        uint8_t msg_id = 0;
+        spec_revision revision = REV_2_0;
+        port_data_role data_role = DFP;
+        port_power_role power_role = SINK;
+        pkt_type type = GOOD_CRC;
     };
 
     struct pdo {
@@ -46,9 +83,12 @@ namespace proto_def
         uint32_t voltage_min        = 0;
         uint32_t voltage_max        = 0;
         uint32_t current_min        = 0;
+        uint32_t current_max        = 0;
         uint32_t power_min          = 0;
         uint32_t power_max          = 0;
     };
+
+    typedef std::array<pdo, 7> pdos;
 }
 
 namespace protocol
@@ -56,8 +96,12 @@ namespace protocol
     class tcpm
     {
     public:
-        tcpm(device::tcpc& _device);
-        int on_pkt_rx();
+        explicit tcpm(device::tcpc& _device);
+        esp_err_t on_pkt_rx();
+
+    private:
+
+
 
     private:
         device::tcpc& port_dev;
