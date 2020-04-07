34a7753b1b4f5b71e97d2b2111a3b707a1a22656
Jackson Ming Hu <huming2207@gmail.com>
2020-04-07 11:23:57 +1000

    pdo: no more macros

--- main/tcpm/power_data_obj.cpp
+++ main/tcpm/power_data_obj.cpp
@@ -1,40 +1,11 @@
 #include <cmath>
 #include <esp_log.h>
-#include <esp_bit_defs.h>
-
 #include "power_data_obj.hpp"
 
 using namespace protocol;
 
 #define TAG "tcpm_pdo"
 
-#define SET_MAXIMUM_CURRENT(val)                ((uint32_t)(((uint16_t)((val) / 10U) & 0x3ffU) << 0U))
-#define SET_OPERATING_CURRENT(val)              ((uint32_t)(((uint16_t)((val) / 10U) & 0x3ffU) << 10U))
-#define SET_UNCHUNKED_MSG(bit)                  ((uint32_t)(((uint8_t)(bit)) << 23U))
-#define SET_NO_USB_SUSPEND(bit)                 ((uint32_t)(((uint8_t)(bit)) << 24U))
-#define SET_USB_COMM_CAP(bit)                   ((uint32_t)(((uint8_t)(bit)) << 25U))
-#define SET_CAP_MISMATCH(bit)                   ((uint32_t)(((uint8_t)(bit)) << 26U))
-#define SET_GIVEBACK(bit)                       ((uint32_t)(((uint8_t)(bit)) << 27U))
-#define SET_OBJ_POSITION(pos)                   ((uint32_t)(((uint8_t)(pos) & 0b111U) << 28U))
-
-#define GET_DUAL_ROLE_POWER(pdo_word)           ((((pdo_word) & BIT(29U)) != 0))
-#define GET_USB_SUSPEND_SUPPORT(pdo_word)       ((((pdo_word) & BIT(28U)) != 0))
-#define GET_UNCONSTRAINED_POWER(pdo_word)       ((((pdo_word) & BIT(27U)) != 0))
-#define GET_USB_COMM_CAP(pdo_word)              ((((pdo_word) & BIT(26U)) != 0))
-#define GET_DUAL_ROLE_DATA(pdo_word)            ((((pdo_word) & BIT(25U)) != 0))
-#define GET_UNCHUNKED_MSG_SUPPORT(pdo_word)     ((((pdo_word) & BIT(24U)) != 0))
-#define GET_PEAK_CURRENT(pdo_word)              ((def::peak_current)(((pdo_word) >> 20U) & 0x3U))
-#define GET_FIXED_CURRENT(pdo_word)             ((((pdo_word) >> 10U) & 0x3ffU) * 10U)
-#define GET_FIXED_VOLTAGE(pdo_word)             ((((pdo_word)) & 0x3ffU) * 50U)
-#define GET_VAR_MAX_VOLTAGE(pdo_word)           ((((pdo_word) >> 20U) & 0x3ffU) * 50U)
-#define GET_VAR_MIN_VOLTAGE(pdo_word)           ((((pdo_word) >> 10U) & 0x3ffU) * 50)
-#define GET_VAR_MAX_CURRENT(pdo_word)           ((((pdo_word) & 0x3ffU) * 10))
-#define GET_BATT_MAX_VOLTAGE(pdo_word)          ((((pdo_word) >> 20U) & 0x3ffU) * 50U)
-#define GET_BATT_MIN_VOLTAGE(pdo_word)          ((((pdo_word) >> 10U) & 0x3ffU) * 50U)
-#define GET_BATT_MAX_POWER(pdo_word)            ((((pdo_word) & 0x3ffU) * 10U))
-#define GET_APDO_MAX_CURRENT(pdo_word)          ((((pdo_word) & 0x7fU) * 50U))
-#define GET_APDO_MIN_VOLTAGE(pdo_word)          ((((pdo_word) >> 8U) & 0x7fU) * 100U)
-#define GET_APDO_MAX_VOLTAGE(pdo_word)          ((((pdo_word) >> 16U) & 0x7fU) * 100U)
 
 power_data_obj::power_data_obj(uint32_t pdo_word)
 {
@@ -53,14 +24,14 @@ uint32_t power_data_obj::encode_fixed_pdo_request(int obj_pos, int op_current)
 
     uint32_t pdo_word = 0;
 
-    pdo_word |= SET_OBJ_POSITION(obj_pos);
-    pdo_word |= SET_GIVEBACK(1);
-    pdo_word |= SET_CAP_MISMATCH(0);
-    pdo_word |= SET_USB_COMM_CAP(0);
-    pdo_word |= SET_NO_USB_SUSPEND(0);
-    pdo_word |= SET_UNCHUNKED_MSG(0);
-    pdo_word |= SET_MAXIMUM_CURRENT(current);
-    pdo_word |= SET_OPERATING_CURRENT(op_current);
+    pdo_word |= set_obj_position(obj_pos);
+    pdo_word |= set_giveback(1);
+    pdo_word |= set_cap_mismatch(0);
+    pdo_word |= set_usb_comm_cap(0);
+    pdo_word |= set_no_usb_suspend(0);
+    pdo_word |= set_unchunked_msg(0);
+    pdo_word |= set_fixed_current(current);
+    pdo_word |= set_fixed_voltage(voltage_max);
 
     return pdo_word;
 }
@@ -84,37 +55,37 @@ esp_err_t power_data_obj::decode_pdo(uint32_t pdo_word)
     // Step 2: Parse according to different types
     switch(pdo_type) {
         case def::FIXED_PDO: {
-            dual_role_power         = GET_DUAL_ROLE_POWER(pdo_word);
-            suspend_support         = GET_USB_SUSPEND_SUPPORT(pdo_word);
-            unconstrained_power     = GET_UNCONSTRAINED_POWER(pdo_word);
-            usb_comm                = GET_USB_COMM_CAP(pdo_word);
-            dual_role_data          = GET_DUAL_ROLE_DATA(pdo_word);
-            unchunked_msg_support   = GET_UNCHUNKED_MSG_SUPPORT(pdo_word);
-            peak_curr_lvl           = GET_PEAK_CURRENT(pdo_word);
-            current                 = GET_FIXED_CURRENT(pdo_word); // 10mA step
-            voltage_max             = GET_FIXED_VOLTAGE(pdo_word); // 50mV step
+            dual_role_power         = get_dual_role_power(pdo_word);
+            suspend_support         = get_usb_suspend_support(pdo_word);
+            unconstrained_power     = get_unconstrained_power(pdo_word);
+            usb_comm                = get_usb_comm_cap(pdo_word);
+            dual_role_data          = get_dual_role_data(pdo_word);
+            unchunked_msg_support   = get_unchunked_msg_support(pdo_word);
+            peak_curr_lvl           = get_peak_current(pdo_word);
+            current                 = get_fixed_current(pdo_word); // 10mA step
+            voltage_max             = get_fixed_voltage(pdo_word); // 50mV step
             voltage_min             = voltage_max;
             break;
         }
 
         case def::BATTERY_PDO: {
-            voltage_max             = GET_BATT_MAX_VOLTAGE(pdo_word);
-            voltage_min             = GET_BATT_MIN_VOLTAGE(pdo_word);
-            power                   = GET_BATT_MAX_POWER(pdo_word);
+            voltage_max             = get_batt_max_voltage(pdo_word);
+            voltage_min             = get_batt_min_voltage(pdo_word);
+            power                   = get_batt_max_power(pdo_word);
             break;
         }
 
         case def::VARIABLE_PDO: {
-            voltage_max             = GET_VAR_MAX_VOLTAGE(pdo_word);
-            voltage_min             = GET_VAR_MIN_VOLTAGE(pdo_word);
-            current                 = GET_VAR_MAX_CURRENT(pdo_word);
+            voltage_max             = get_var_max_voltage(pdo_word);
+            voltage_min             = get_var_min_voltage(pdo_word);
+            current                 = get_var_max_current(pdo_word);
             break;
         }
 
         case def::AUGMENTED_PDO: {
-            current                 = GET_APDO_MAX_CURRENT(pdo_word);
-            voltage_min             = GET_APDO_MIN_VOLTAGE(pdo_word);
-            voltage_max             = GET_APDO_MAX_VOLTAGE(pdo_word);
+            current                 = get_apdo_max_current(pdo_word);
+            voltage_min             = get_apdo_min_voltage(pdo_word);
+            voltage_max             = get_apdo_max_voltage(pdo_word);
             break;
         }
     }
--- main/tcpm/power_data_obj.hpp
+++ main/tcpm/power_data_obj.hpp
@@ -2,6 +2,8 @@
 
 #include <cstdint>
 #include <esp_err.h>
+#include <esp_bit_defs.h>
+
 
 #include "tcpm_def.hpp"
 
@@ -13,28 +15,156 @@ namespace protocol
         power_data_obj() = default;
         power_data_obj(power_data_obj const &) = default;
         explicit power_data_obj(uint32_t pdo_word);
-
         uint32_t encode_fixed_pdo_request(int obj_pos = 0, int op_current = -1);
         uint32_t encode_augmented_pdo(uint8_t obj_pos = 0);
-
         esp_err_t decode_pdo(uint32_t pdo_word);
-
-        bool operator== (power_data_obj &other);
-        bool operator> (power_data_obj &other);
-        bool operator< (power_data_obj &other);
+        bool operator==(power_data_obj &other);
+        bool operator>(power_data_obj &other);
+        bool operator<(power_data_obj &other);
 
     public:
-        bool suspend_support        = false;
-        bool unconstrained_power    = false;
-        bool usb_comm               = false;
-        bool dual_role_data         = false;
-        bool dual_role_power        = false;
-        bool unchunked_msg_support  = false;
-        def::pdo_type pdo_type      = def::FIXED_PDO;
-        def::peak_current peak_curr_lvl  = def::PEAK_CURRENT_NONE;
-        uint32_t voltage_min        = 0;
-        uint32_t voltage_max        = 0;
-        uint32_t current            = 0;
-        uint32_t power              = 0;
+        bool suspend_support = false;
+        bool unconstrained_power = false;
+        bool usb_comm = false;
+        bool dual_role_data = false;
+        bool dual_role_power = false;
+        bool unchunked_msg_support = false;
+        def::pdo_type pdo_type = def::FIXED_PDO;
+        def::peak_current peak_curr_lvl = def::PEAK_CURRENT_NONE;
+        uint32_t voltage_min = 0;
+        uint32_t voltage_max = 0;
+        uint32_t current = 0;
+        uint32_t power = 0;
+
+    private:
+        constexpr static uint32_t set_fixed_current(uint32_t curr_ma)
+        {
+            return ((uint32_t) (((uint16_t) ((curr_ma) / 10U) & 0x3ffU) << 0U));
+        };
+
+        constexpr static uint32_t set_fixed_voltage(uint32_t volt_mv)
+        {
+            return ((uint32_t) (((uint16_t) ((volt_mv) / 10U) & 0x3ffU) << 10U));
+        };
+
+        constexpr static uint32_t set_unchunked_msg(uint8_t bit)
+        {
+            return ((uint32_t) (((uint8_t) (bit)) << 23U));
+        };
+
+        constexpr static uint32_t set_no_usb_suspend(uint8_t bit)
+        {
+            return ((uint32_t) (((uint8_t) (bit)) << 24U));
+        };
+
+        constexpr static uint32_t set_usb_comm_cap(uint8_t bit)
+        {
+            return ((uint32_t) (((uint8_t) (bit)) << 25U));
+        };
+
+        constexpr static uint32_t set_cap_mismatch(uint8_t bit)
+        {
+            return ((uint32_t) (((uint8_t) (bit)) << 26U));
+        };
+
+        constexpr static uint32_t set_giveback(uint8_t bit)
+        {
+            return ((uint32_t) (((uint8_t) (bit)) << 27U));
+        };
+
+        constexpr static uint32_t set_obj_position(uint8_t pos)
+        {
+            return ((uint32_t) (((uint8_t) (pos) & 0b111U) << 28U));
+        };
+
+        constexpr static uint32_t get_dual_role_power(uint32_t pdo_word)
+        {
+            return ((((pdo_word) & BIT(29U)) != 0));
+        };
+
+        constexpr static uint32_t get_usb_suspend_support(uint32_t pdo_word)
+        {
+            return ((((pdo_word) & BIT(28U)) != 0));
+        };
+
+        constexpr static uint32_t get_unconstrained_power(uint32_t pdo_word)
+        {
+            return ((((pdo_word) & BIT(27U)) != 0));
+        };
+
+        constexpr static uint32_t get_usb_comm_cap(uint32_t pdo_word)
+        {
+            return ((((pdo_word) & BIT(26U)) != 0));
+        };
+
+        constexpr static uint32_t get_dual_role_data(uint32_t pdo_word)
+        {
+            return ((((pdo_word) & BIT(25U)) != 0));
+        };
+
+        constexpr static uint32_t get_unchunked_msg_support(uint32_t pdo_word)
+        {
+            return ((((pdo_word) & BIT(24U)) != 0));
+        };
+
+        constexpr static def::peak_current get_peak_current(uint32_t pdo_word)
+        {
+            return ((def::peak_current) (((pdo_word) >> 20U) & 0x3U));
+        };
+
+        constexpr static uint32_t get_fixed_current(uint32_t pdo_word)
+        {
+            return ((((pdo_word) >> 10U) & 0x3ffU) * 10U);
+        };
+
+        constexpr static uint32_t get_fixed_voltage(uint32_t pdo_word)
+        {
+            return ((((pdo_word)) & 0x3ffU) * 50U);
+        };
+
+        constexpr static uint32_t get_var_max_voltage(uint32_t pdo_word)
+        {
+            return ((((pdo_word) >> 20U) & 0x3ffU) * 50U);
+        };
+
+        constexpr static uint32_t get_var_min_voltage(uint32_t pdo_word)
+        {
+            return ((((pdo_word) >> 10U) & 0x3ffU) * 50);
+        };
+
+        constexpr static uint32_t get_var_max_current(uint32_t pdo_word)
+        {
+            return ((((pdo_word) & 0x3ffU) * 10));
+        };
+
+        constexpr static uint32_t get_batt_max_voltage(uint32_t pdo_word)
+        {
+            return ((((pdo_word) >> 20U) & 0x3ffU) * 50U);
+        };
+
+        constexpr static uint32_t get_batt_min_voltage(uint32_t pdo_word)
+        {
+            return ((((pdo_word) >> 10U) & 0x3ffU) * 50U);
+        };
+
+        constexpr static uint32_t get_batt_max_power(uint32_t pdo_word)
+        {
+            return ((((pdo_word) & 0x3ffU) * 10U));
+        };
+
+        constexpr static uint32_t get_apdo_max_current(uint32_t pdo_word)
+        {
+            return ((((pdo_word) & 0x7fU) * 50U));
+        };
+
+        constexpr static uint32_t get_apdo_min_voltage(uint32_t pdo_word)
+        {
+            return ((((pdo_word) >> 8U) & 0x7fU) * 100U);
+        };
+
+        constexpr static uint32_t get_apdo_max_voltage(uint32_t pdo_word)
+        {
+            return ((((pdo_word) >> 16U) & 0x7fU) * 100U);
+        };
     };
 }
